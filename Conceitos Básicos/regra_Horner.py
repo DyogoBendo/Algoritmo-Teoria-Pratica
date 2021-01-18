c60ee077e9e48a3a7379b490606f93729a36dc4a
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-01-18 11:01:06 -0300

    Problema 2-3, b: algoritmo ingênuo

--- Conceitos Básicos/regra_Horner.py
+++ Conceitos Básicos/regra_Horner.py
@@ -9,6 +9,16 @@ def regra_horner(x, coeficientes):
     return y
 
 
+def algoritmo_ingenuo(x, coeficientes):
+    y = 0
+    for i in range(len(coeficientes) - 1, -1, -1):
+        valor_exponente = 1
+        for j in range(i):
+            valor_exponente *= x
+        y += coeficientes[i] * valor_exponente
+    return y
+
+
 if __name__ == '__main__':
     coeficientes = list(map(int, input('Insira os coeficientes de um polinomio: ').split(', ')))
     lista_coeficientes = ''
@@ -31,3 +41,5 @@ if __name__ == '__main__':
     coeficientes.reverse()
     x = int(input('Insira um valor de x: '))
     print(f'Resultado: {regra_horner(x, coeficientes)}')
+    print(f'Resultado: {algoritmo_ingenuo(x, coeficientes)}')
+
