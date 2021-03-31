c58ad53a3945fb80c91bf9bdcdd96174492e55c8
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-03-31 17:48:23 -0300

    min-heapfy

--- Ordenação por Heap/heap.py
+++ Ordenação por Heap/heap.py
@@ -26,9 +26,40 @@ def max_heapfy(A, i):
         max_heapfy(A, maior)        
 
 
+
+def min_heapfy(A, i):
+    l = 2 * i
+    r = 2*i + 1
+    
+    if l < len(A):
+        if A[l] < A[i]:
+            menor = l
+        else:
+            menor = i
+    else:
+        menor = i
+
+    if r < len(A):
+        if A[r] < A[i]:
+            menor = r
+        else:
+            menor = i
+    else:
+        menor = i
+    
+    if menor != i:
+        A[i], A[menor] = A[menor], A[i]
+        max_heapfy(A, menor)        
+
+# Ambos possuem o mesmo tempo de execução
+
 if __name__ == "__main__":        
     heap = [5, 0, 3, 1, 2]
     print(heap)
         
-    max_heapfy(heap, 1)
+    max_heapfy(heap, 1) 
+    print(heap)
+    
+    min_heapfy(heap, 1)
+    print(heap)
     
