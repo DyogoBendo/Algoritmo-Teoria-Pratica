4f89f36fa6fd4edf139775eecca72f1b6ff39dd2
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-04-07 11:11:18 -0300

    algoritmo do quicksort

---
+++ Quicksort/quicksort.py
@@ -0,0 +1,28 @@
+from random import randint
+
+def quicksort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q - 1)
+        quicksort(A, q + 1, r)
+
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
+
+if __name__ == "__main__":
+    A = [randint(0, 100) for _ in range(10)]
+    
+    print(A)
+    
+    quicksort(A, 0, len(A) - 1)
+    
+    print(A)
