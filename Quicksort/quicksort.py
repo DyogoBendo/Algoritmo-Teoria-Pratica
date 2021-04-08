0da0d086f90867529cf9e85ef22755cdd0771030
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-04-08 20:56:25 -0300

    hoare partition

--- Quicksort/quicksort.py
+++ Quicksort/quicksort.py
@@ -26,7 +26,39 @@ def randomized_partition(A, p, r):
     A[r], A[i] = A[i], A[r]  # trocamos a ultima posicao por um elemento aleatorio
     return partition(A, p, r)
     
-
+def hoare_partition(A, p, r):
+    print("hey ey")
+    x = A[p]
+    i = p - 1
+    j = r + 1
+    
+    while True:        
+        while True:
+            j -= 1
+            if A[j] <= x:
+                break
+        print("j:", A[j])
+        print(A)
+        print()
+        
+        while True:
+            i += 1
+            if A[i] >= x:
+                break
+        print("i: ", A[i])        
+        print(A)
+        print()
+        
+        if i < j:
+            A[i], A[j] = A[j], A[i]
+            print("troca: ", A)
+            print()
+        else:
+            print(A)
+            print()
+            return j
+    
+    
 
 def quicksort_decreasing(A, p, r):
     if p < r:
@@ -49,7 +81,29 @@ def partition_decreasing(A, p, r):
     
     return i + 1
     
-
+def partition_2(a, low, high):
+ 
+    pivot = a[low]
+    (i, j) = (low - 1, high + 1)
+ 
+    while True:
+ 
+        while True:
+            i = i + 1
+            if a[i] >= pivot:
+                break
+ 
+        while True:
+            j = j - 1
+            if a[j] <= pivot:
+                break
+ 
+        if i >= j:
+            print(a)
+            return j
+ 
+        a[i], a[j] = a[j], a[i]
+                
 if __name__ == "__main__":
     A = list({randint(0, 100)  for _ in range(11)})
     
@@ -60,3 +114,9 @@ if __name__ == "__main__":
     
     quicksort_decreasing(A, 0, len(A) - 1)    
     print(A)
+    
+    print(hoare_partition([13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21], 0, 11))
+    print()
+    
+    print(partition_2([13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21], 0, 11))
+    
