79230850d5ca630fcfd6f9a7377a9dcf716222cd
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-12 18:46:54 -0300

    Selecionar o ienésimo elemento

--- Medianas e Estatísticas de Ordem/max_min.py
+++ Medianas e Estatísticas de Ordem/max_min.py
@@ -1,5 +1,38 @@
 from random import randint
 
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    
+    if A[p] == A[r - 1]:  # condição para quando todos os elementos são iguais
+        i = (p + r) // 2
+    
+    return i + 1
+
+def randomized_partition(A, p, r):
+    i = randint(p, r)
+    A[r], A[i] = A[i], A[r]  # trocamos a ultima posicao por um elemento aleatorio
+    return partition(A, p, r)
+
+
+def randomized_select(A, p, r, i):
+    if p ==r:
+        return A[p]
+    q = randomized_partition(A, p, r)
+    k = q - p + 1
+    if i == k:
+        return A[q]
+    elif i < k:
+        return randomized_select(A, p, q - 1, i)
+    else:
+        return randomized_select(A, q + 1, r, i - k)
+
+
 def min(A):
     min = A[0]
     for i in A:         
@@ -54,8 +87,10 @@ if __name__ == "__main__":
     print(A)
     print(min(A))
     print(min_max_simultaneos(A))
+    print(randomized_select(A, 0, len(A) - 1, 5))
 
     print()
 
     print(B)
     print(min_max_simultaneos(B))
+    
