b57f29221a7e1741c85327dfa8d9a8dcaa62c371
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-12 17:43:34 -0300

    Maximo e mínimo simultâneos

--- Medianas e Estatísticas de Ordem/max_min.py
+++ Medianas e Estatísticas de Ordem/max_min.py
@@ -9,8 +9,53 @@ def min(A):
     return min
 
 
+def min_max_simultaneos(A):
+    if len(A) % 2 != 0:
+        min = max = A[0]
+        for i in range(1, len(A) - 1):
+            a = A[i]
+            b = A[i + 1]
+
+            if a > b: 
+                if a > max:
+                    max = a
+                if b < min:
+                    min = b
+            else:
+                if b > max:
+                    max = b
+                if a < min:
+                    min = a
+    else:
+        min = A[0]
+        max = A[1]
+
+        for i in range(2, len(A) - 1):
+            a = A[i]
+            b = A[i + 1]
+
+            if a > b: 
+                if a > max:
+                    max = a
+                if b < min:
+                    min = b
+            else:
+                if b > max:
+                    max = b
+                if a < min:
+                    min = a
+    return (min, max)
+
+
 if __name__ == "__main__":
     A = [randint(0, 100) for _ in range(10)]
+    B = [randint(0, 100) for _ in range(11)]
 
     print(A)
     print(min(A))
+    print(min_max_simultaneos(A))
+
+    print()
+
+    print(B)
+    print(min_max_simultaneos(B))
