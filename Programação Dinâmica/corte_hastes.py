b61191aa1a8ed84077e1a9b14b00b2f06259460d
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-22 11:14:06 -0300

    calculando tempo de execução

--- Programação Dinâmica/corte_hastes.py
+++ Programação Dinâmica/corte_hastes.py
@@ -1,4 +1,5 @@
 from math import inf
+import time
 
 # Resolução não dinâmica
 
@@ -15,7 +16,7 @@ def cut_rod(p, n):  # p é a tabela de preços, e n o tamanho do corte máximo
 
 # Programação dinâmica de cima para baixo
 def memoized_cut_rod(p, n):
-    r = [- inf for _ in range(n)]    
+    r = [- inf for _ in range(n + 1)]    
     return memoized_cut_rod_aux(p, n, r)
 
 
@@ -24,13 +25,42 @@ def memoized_cut_rod_aux(p, n, r):
         return r[n]
     q = 0 if n == 0 else - inf
     for i in range(n):
-        q = max(q, p[i] + memoized_cut_rod_aux(p, n - i, r))
+        q = max(q, p[i] + memoized_cut_rod_aux(p, n - (i + 1), r))
     r[n] = q  # salvamos a solução encontrada para não ser necessário recálculo
 
     return q
 
 
+def bottom_up_cut_rod(p, n):
+    r = [-inf for _ in range(n + 1)]
+    r[0] = 0    
+
+    for j in range(1, n + 1):
+        q = -inf
+        for i in range(0, j):            
+            q = max(q, p[i] + r[j - (i + 1)])  # calculamos todos os subproblemas anteriores de n, antes de calcular o problema n
+        r[j] = q    
+    return r[n]
+
+
 if __name__ == "__main__":
-    precos = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]    
-    for i in range(1, 11):
+    precos = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 45, 50, 54, 60, 71, 80, 85, 87, 90, 99]
+    start_time = time.time()
+    print("Calculando de forma ingênua: ")    
+    for i in range(0, 21):
         print(f"Com tamanho {i} a receita total é: R$ {cut_rod(precos, i):.2f}")
+    print("--- %s seconds ---" % (time.time() - start_time))
+        
+    print("-" * 50)
+    start_time = time.time()
+    print("Calcula de cima para baixo: ")        
+    for i in range(0, 21):
+        print(f"Com tamanho {i} a receita total é: R$ {memoized_cut_rod(precos, i):.2f}")
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    print("-" * 50)        
+    start_time = time.time()
+    print("Calculando de baixo para cima: ")
+    for i in range(0, 21):        
+        print(f"Com tamanho {i} a receita total é: R$ {bottom_up_cut_rod(precos, i):.2f}")
+    print("--- %s seconds ---" % (time.time() - start_time))
