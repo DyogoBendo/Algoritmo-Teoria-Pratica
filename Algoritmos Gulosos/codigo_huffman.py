05742c7ce97e67f31759dfdeeb9e5bb77a0cd778
dyogobendo <dyogoromagnabendo@gmail.com>
2021-05-07 10:39:55 -0300

    codigo huffman

--- Algoritmos Gulosos/codigo_huffman.py
+++ Algoritmos Gulosos/codigo_huffman.py
@@ -1,13 +1,32 @@
-from ..utils.binary_tree import ArvoreBinaria
-from ..utils.queue import Fila
+class Fila():
+    def __init__(self, fila = []) -> None:
+        self.fila = fila
+        self.inicio = fila[0] if fila else 0
+        self.fim = len(fila) if fila else 0
+    
+    def enqueue(self, x):
+        self.fila.append(x)        
+        self.fim += 1
+    
+    def dequeue(self):            
+        if self.fim > 0:
+            self.fim -= 1
+            return self.fila.pop(0)
+        else:
+            raise Exception("A fila já está vazia")
 
 class Node():    
-    def __init__(self) -> None:
+    def __init__(self, v = None, freq=0) -> None:
         self.left = None
         self.right = None
-        self.freq = 0
+        self.freq = freq
+        self.v = v
+    
+    def __str__(self) -> str:
+        txt = f"Valor: {self.v}, Frequência: {self.freq}"
+        return txt
 
-def huffman(C):
+def huffman(C) -> Node:
     n = len(C)
     Q = Fila(C)
     for _ in range(n - 1):      
@@ -17,4 +36,16 @@ def huffman(C):
         z.freq = x.freq + y.freq
         Q.enqueue(z)
     return Q.dequeue()  
+
+
+if __name__ == "__main__":    
+    a = Node("a", 45)
+    b = Node("b", 13)
+    c = Node("c", 12)
+    d = Node("d", 16)
+    e = Node("e", 9)
+    f = Node("f", 5)    
+    alfabeto = [a, b, c, d, e, f]
+    raiz = huffman(alfabeto)
+    print(raiz, raiz.left, raiz.right)
 
