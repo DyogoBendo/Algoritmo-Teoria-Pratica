ed90a533a37707b021adf0c37ddfdc9af7a69f42
dyogobendo <dyogoromagnabendo@gmail.com>
2021-05-10 10:38:43 -0300

    melhorando output

--- Algoritmos Gulosos/programacao_tarefas.py
+++ Algoritmos Gulosos/programacao_tarefas.py
@@ -1,8 +1,12 @@
 class Node():    
-    def __init__(self, d, w) -> None:        
+    def __init__(self, d, w, i) -> None:        
         self.d = d
         self.w = w                
-    
+        self.i = i
+
+    def __str__(self) -> str:        
+        return str(self.i)
+
     def __eq__(self, other):
         return self.d == other.d
     def __lt__(self, other):
@@ -13,15 +17,12 @@ class Node():
 def greedy(M: list):
     A = []  
     cost = 0  
-    t = 0  
-    i = 0
-    for x in M:
-        i += 1
+    t = 0      
+    for x in M:        
         size = len(A)
         t = x.d if x.d > t else t
-        if t >= size + 1:
-            a = f"a{i}"
-            A.append(a)
+        if t >= size + 1:            
+            A.append(x)
         else:
             cost += x.w
     A.sort()
@@ -30,16 +31,19 @@ def greedy(M: list):
 
 
 if __name__ == "__main__":
-    a1 = Node(4, 70)
-    a2 = Node(2, 60)
-    a3 = Node(4, 50)
-    a4 = Node(3, 40)
-    a5 = Node(1, 30)
-    a6 = Node(4, 20)
-    a7 = Node(6, 10)
+    a1 = Node(4, 70, 1)
+    a2 = Node(2, 60, 2)
+    a3 = Node(4, 50, 3)
+    a4 = Node(3, 40, 4)
+    a5 = Node(1, 30, 5)
+    a6 = Node(4, 20, 6)
+    a7 = Node(6, 10, 7)
 
     tarefas = [a1, a2, a3, a4, a5, a6, a7]
 
     to_do, cost = greedy(tarefas)
-    print("Tarefas que precisam ser feitas:",to_do)
+    txt = ""
+    for x in to_do:
+        txt += str(x.i) + " "
+    print("Tarefas que precisam ser feitas:", txt)
     print("Custo em detrimento das tarefas não feitas:",cost)
