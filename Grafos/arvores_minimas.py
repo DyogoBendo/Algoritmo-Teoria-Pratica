ffd2789aae1d123ad436bf52d38f97af630000ee
dyogobendo <dyogoromagnabendo@gmail.com>
2021-05-21 09:42:58 -0300

    aceitando qualquer tipo como vértices

--- Grafos/arvores_minimas.py
+++ Grafos/arvores_minimas.py
@@ -2,13 +2,20 @@ from collections import defaultdict
 
 
 class Graph:
-    def __init__(self, vertices) -> None:
-        self.V = vertices
+    def __init__(self) -> None:
+        self.V = []
         self.graph = []
     
     def addEdge(self, u, v, w):
         self.graph.append([u, v, w])
+        self.addVertices(u)
+        self.addVertices(v)
     
+
+    def addVertices(self, v):
+        if v not in self.V:
+            self.V.append(v)
+
     def find(self, parent, i):
         if parent[i] == i:
             return i
@@ -34,14 +41,14 @@ class Graph:
         e = 0
         self.graph = sorted(self.graph, key=lambda item:item[2])
 
-        parent = []
-        rank = []
+        parent = defaultdict()
+        rank = defaultdict()
 
-        for node in range(self.V):
-            parent.append(node)
-            rank.append(0)
+        for node in self.V:            
+            parent[node] = node
+            rank[node] = 0            
         
-        while e < self.V -1:
+        while e < len(self.V) -1:
             u, v, w = self.graph[i]
             i += 1
             x = self.find(parent, u)
@@ -61,11 +68,21 @@ class Graph:
 
 
 if __name__ == '__main__':
-    g = Graph(4)
-    g.addEdge(0, 1, 10)
-    g.addEdge(0, 2, 6)
-    g.addEdge(0, 3, 5)
-    g.addEdge(1, 3, 15)
-    g.addEdge(2, 3, 4)
+    g = Graph()
+    g.addEdge('a', 'b', 4)
+    g.addEdge('a', 'h', 8)
+    g.addEdge('b', 'c', 8)
+    g.addEdge('b', 'h', 11)
+    g.addEdge('c', 'd', 7)
+    g.addEdge('c', 'i', 2)
+    g.addEdge('c', 'f', 4)
+    g.addEdge('d', 'e', 9)
+    g.addEdge('d', 'f', 14)
+    g.addEdge('e', 'f', 10)
+    g.addEdge('f', 'g', 2)
+    g.addEdge('g', 'i', 6)
+    g.addEdge('g', 'h', 1)
+    g.addEdge('h', 'i', 7)
+    
 
     g.KruskaMST()
