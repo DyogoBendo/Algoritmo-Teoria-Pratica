4cc5754db346e3c45bc3be0cbe23fca387f448ff
dyogobendo <dyogoromagnabendo@gmail.com>
2021-05-21 11:09:00 -0300

    algoritmo de prim

--- Grafos/arvores_minimas.py
+++ Grafos/arvores_minimas.py
@@ -1,7 +1,138 @@
 from collections import defaultdict
+from math import inf
 
 
-class Graph:
+class Heap():
+    def __init__(self) -> None:
+        self.array = []
+        self.size = 0
+        self.pos = []
+    
+
+    def newMinHeapNode(self, v, dist):
+        minHeapNode = [v, dist]
+        return minHeapNode
+    
+
+    def swapMinHeapNode(self, a, b):        
+        self.array[a], self.array[b] = self.array[b], self.array[a]
+
+
+    def minHeapfy(self, idx):
+        smallest = idx
+        left = 2 * idx + 1
+        right = 2 * idx + 2
+
+        if left < self.size and self.array[left][1] < self.array[smallest][1]:
+            smallest = left
+        if right < self.size and self.array[right][1] < self.array[smallest][1]:
+            smallest = right
+        
+        if smallest != idx:
+            self.pos[self.array[smallest][0]] = idx            
+            self.pos[self.array[idx][0]] = smallest
+
+            self.swapMinHeapNode(smallest, idx)
+            self.minHeapfy(smallest)
+
+
+    def extractMin(self):
+        if self.isEmpty():
+            return
+        
+        root = self.array[0]
+        lastNode = self.array[self.size - 1]
+        self.array[0] = lastNode
+
+        self.pos[lastNode[0]] = 0
+        self.pos[root[0]] = self.size -1
+
+        self.size -= 1
+        self.minHeapfy(0)
+
+        return root
+    
+
+    def isEmpty(self):
+        return True if self.size == 0 else False
+    
+
+    def decreaseKey(self, v, dist):
+        i = self.pos[v]
+        self.array[i][1] = dist
+
+        while i > 0 and self.array[i][1] < self.array[(i - 1) / 2][1]:
+            self.pos[self.array[i][0]] = (i - 1)/2
+            self.pos[self.array[(i - 1) / 2][0]] = i
+            self.swapMinHeapNode(i, (i -1)/2)
+            i = (i - 1)/2
+    
+    def isInMinHeap(self, v):
+        if self.pos[v] < self.size:
+            return True
+        return False
+
+
+class PrimGraph():
+    def __init__(self, V) -> None:
+        self.V = V
+        self.graph = defaultdict(list)
+    
+
+    def addEdge(self, src, dest, weight):
+        newNode = [dest, weight]
+        self.graph[src].insert(0, newNode)
+
+        newNode = [src, weight]
+        self.graph[dest].insert(0, newNode)
+
+
+    def primMST(self):
+        V = self.V
+        key = []
+        parent = []
+        minHeap = Heap()
+        for v in range(V):
+            parent.append(-1)
+            key.append(inf)
+            minHeap.array.append(minHeap.newMinHeapNode(v, key[v]))
+            minHeap.pos.append(v)
+        
+
+        minHeap.pos[0] = 0
+        key[0] = 0
+        minHeap.decreaseKey(0, key[0])
+  
+        # Initially size of min heap is equal to V
+        minHeap.size = V;
+  
+        # In the following loop, min heap contains all nodes
+        # not yet added in the MST.
+        while minHeap.isEmpty() == False:
+  
+            # Extract the vertex with minimum distance value
+            newHeapNode = minHeap.extractMin()
+            u = newHeapNode[0]
+  
+            # Traverse through all adjacent vertices of u 
+            # (the extracted vertex) and update their 
+            # distance values
+            for pCrawl in self.graph[u]:
+  
+                v = pCrawl[0]
+  
+                # If shortest distance to v is not finalized 
+                # yet, and distance to v through u is less than
+                # its previously calculated distance
+                if minHeap.isInMinHeap(v) and pCrawl[1] < key[v]:
+                    key[v] = pCrawl[1]
+                    parent[v] = u
+  
+                    # update distance value in min heap also
+                    minHeap.decreaseKey(v, key[v])
+
+
+class KruskalGraph:
     def __init__(self) -> None:
         self.V = []
         self.graph = []
@@ -68,7 +199,7 @@ class Graph:
 
 
 if __name__ == '__main__':
-    g = Graph()
+    g = KruskalGraph()
     g.addEdge('a', 'b', 4)
     g.addEdge('a', 'h', 8)
     g.addEdge('b', 'c', 8)
